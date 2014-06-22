5f2c7570d9f981793ed2a7243172301f8a7c2b02
Larry Davis <lazdnet@gmail.com>
2014-06-22 13:26:14 -0700

    Use the simplewebrtc dataChannel for editor communications, add support for undo/redo

--- source/js/editor.js
+++ source/js/editor.js
@@ -8,10 +8,10 @@ require('codemirror/mode/clike/clike');
 require('codemirror/mode/ruby/ruby');
 require('codemirror/mode/python/python');
 
-// Include CodeMirror theme
-// require('code-mirror/theme/ambiance');
+module.exports = function(simplewebRTC) {
+  // Get the webrtc instance from the SimpleWebRTC instance
+  var webRTC = simplewebRTC.webrtc;
 
-module.exports = function(webrtc) {
   var editor = CodeMirror.fromTextArea(document.getElementById('cc-Code'), {
     dragDrop: false, // Too hard to handle in code
     mode: 'javascript',
@@ -25,28 +25,7 @@ module.exports = function(webrtc) {
 
     editor.setOption('mode', language);
 
-    webrtc.sendDirectlyToAll('editor', 'changeLanguage', language);
-  });
-
-  webrtc.on('peerStreamAdded', function(peer) {
-    // Works, but connections drop when connecting clients at precisely the same time
-    broadcastEditorContents();
-
-    // Causes the random connection drop bug
-    // webrtc.sendDirectlyToAll('editor', 'open', 'sesame');
-    // webrtc.sendDirectlyToAll('editor', 'sesame', 'open');
-
-    // Causes the random connection drop bug
-    // webrtc.sendDirectlyToAll('editor', 'open', 'sesame');
-
-    // Causes the random connection drop bug
-    // Try to get that data channel open
-    // Don't call methods on the peer object, it causes bugs!
-    // webrtc.sendDirectlyToAll('editor', 'hello');
-
-    // Causes the random connection drop bug
-    // Open the editor channel with the peer
-    // peer.getDataChannel('editor');
+    webRTC.sendDirectlyToAll('simplewebrtc', 'changeLanguage', language);
   });
 
   function broadcastEditorContents() {
@@ -57,17 +36,17 @@ module.exports = function(webrtc) {
       console.log('Sending editor contents to peers!');
 
       // When the channel is open, send the current contents of the editor to everyone
-      webrtc.sendDirectlyToAll('editor', 'refresh', editorContents);
+      webRTC.sendDirectlyToAll('simplewebrtc', 'refresh', editorContents);
 
       // Send the current language
-      webrtc.sendDirectlyToAll('editor', 'changeLanguage', language);
+      webRTC.sendDirectlyToAll('simplewebrtc', 'changeLanguage', language);
     }
   }
 
-  webrtc.on('channelOpen', function(channel) {
+  webRTC.on('channelOpen', function(channel) {
     console.log('%s opened!', channel.label);
 
-    if (channel.label === 'editor') {
+    if (channel.label === 'simplewebrtc') {
       // When the editor channel opens, broadcast the goodies
       // This is a bit redundant because we tried to do that when peerStreamAdded
       // But it turns out all good
@@ -75,16 +54,16 @@ module.exports = function(webrtc) {
     }
   });
 
-  webrtc.on('channelClose', function(channel) {
+  webRTC.on('channelClose', function(channel) {
     console.warn('%s closed!', channel.label);
   });
 
-  webrtc.on('channelError', function(label, error) {
+  webRTC.on('channelError', function(label, error) {
     console.error('Error on %s: %s', label, error);
   });
 
-  webrtc.on('channelMessage', function (peer, label, data) {
-    if (label === 'editor') {
+  webRTC.on('channelMessage', function (peer, label, data) {
+    if (label === 'simplewebrtc') {
       var payload = data.payload;
       if (data.type === 'refresh') {
         console.log('Refreshing editor contents');
@@ -103,23 +82,25 @@ module.exports = function(webrtc) {
     }
   });
 
+  var editorOperations = [
+    '+input',
+    '+delete',
+    'cut',
+    'paste',
+    'undo',
+    'redo'
+  ];
+
   editor.on('change', function(i, op) {
-    /*
-      Bug:
-        Select all, paste. Had an extra pasted item on the remote
-        Undo/Redo
-        Contents get out of sync sometimes... Need a mechanism to resync...
-    */
-    if (op.origin == '+input' || op.origin == '+delete' || op.origin == 'cut' || op.origin == 'paste') {
-      webrtc.sendDirectlyToAll('editor', 'change', op);
+    if (editorOperations.indexOf(op.origin) !== -1) {
+      webRTC.sendDirectlyToAll('simplewebrtc', 'change', op);
     }
-    // socket.emit('refresh', editor.getValue());
   });
 
   editor.on('cursorActivity', function() {
     var selections = editor.doc.listSelections();
 
-    webrtc.sendDirectlyToAll('editor', 'selection', selections);
+    webRTC.sendDirectlyToAll('simplewebrtc', 'selection', selections);
   });
 
   var marks = [];
