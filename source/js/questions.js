55d2beb6eb3794f05d3cbebd094f1112d2ba5f8c
Larry Davis <lazdnet@gmail.com>
2014-06-23 13:16:01 -0700

    Correct expected output

--- source/js/questions.js
+++ source/js/questions.js
@@ -8,7 +8,7 @@ module.exports = [
     name: 'FizzBuzz',
     video: true,
     test: function() {
-      var expected = 'FizzBuzzFizzBuzzBuzzFizzBuzzFizzBuzzFizzBuzzFizzBuzzBuzzFizzFizzBuzzBuzzFizzBuzzFizzBuzzFizzBuzzFizzBuzzBuzzFizzBuzzFizzBuzzFizzBuzzFizzBuzzBuzzFizzFizzBuzzBuzzFizzBuzzFizzBuzzFizzBuzzFizzBuzzBuzzFizzBuzzFizzBuzzFizzBuzzFizzBuzzBuzzFizzFizzBuzzBuzzFizzBuzzFizzBuzzFizzBuzzFizzBuzzBuzzFizzBuzzFizzBuzzFizzBuzzFizzBuzzBuzzFizzFizzBuzzBuzzFizzBuzzFizzBuzzFizzBuzzFizzBuzzBuzzFizzBuzzFizzBuzzFizzBuzzFizzBuzzBuzzFizzFizzBuzzBuzzFizzBuzzFizzBuzzFizzBuzzFizzBuzzBuzzFizzBuzzFizzBuzzFizzBuzzFizzBuzzBuzzFizzFizzBuzzBuzzFizzBuzzFizzBuzzFizzBuzzFizzBuzzBuzzFizzBuzzFizzBuzzFizzBuzzFizzBuzzBuzzFizz';
+      var expected = '12Fizz4BuzzFizz78FizzBuzz11Fizz1314FizzBuzz1617Fizz19BuzzFizz2223FizzBuzz26Fizz2829FizzBuzz3132Fizz34BuzzFizz3738FizzBuzz41Fizz4344FizzBuzz4647Fizz49BuzzFizz5253FizzBuzz56Fizz5859FizzBuzz6162Fizz64BuzzFizz6768FizzBuzz71Fizz7374FizzBuzz7677Fizz79BuzzFizz8283FizzBuzz86Fizz8889FizzBuzz9192Fizz94BuzzFizz9798FizzBuzz';
       fizzBuzz();
       console.assert(console.output === expected, 'Should print the correct output', 'Expected '+expected+'\nGot '+console.output);
     },
